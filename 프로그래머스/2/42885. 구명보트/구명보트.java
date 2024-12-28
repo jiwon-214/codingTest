aa1d6b4cd72d5a240cab7def604ab2784bcde4c4
jwLEE21 <59613778+jiwon-214@users.noreply.github.com>
2024-12-28 19:35:31 +0900

    [level 2] Title: 구명보트, Time: 14.52 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+
+        int light = 0;
+        int heavy = people.length - 1;
+
+        while (light <= heavy) {
+            if(people[light] + people[heavy] <= limit) {
+                light++;
+                heavy--;
+            }
+            else {
+                heavy--;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
